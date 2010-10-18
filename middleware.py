4904dd5b8c8e49f48c0941cf6eed002e30c2ddac
Brandon R. Stoner <monokrome@monokro.me>
2010-10-18 15:49:26 -0600

    Updated to use regular expressions and ignore ports

--- middleware.py
+++ middleware.py
@@ -3,8 +3,10 @@
 
 from django.contrib.sites.models import Site
 from django.conf import settings
+import re
 
 ignore_www_zone = getattr(settings, 'IGNORE_WWW_ZONE', True)
+ignore_server_port = getattr(settings, 'IGNORE_SERVER_PORT', True)
 
 class SiteProviderMiddleware(object):
     def process_request(self, request:
@@ -16,12 +18,16 @@ class SiteProviderMiddleware(object):
         hostname = request.get_host()
 
         # Remove the www from our domain unless requested otherwise
-        if ignore_www_zone and hostname[:4] == 'www.':
-            hostname = hostname[4:]
+        if ignore_www_zone:
+            hostname = re.replace(r'^w{2,3}\d*\.', '', hostname)
+
+        if ignore_server_port:
+            hostname = re.replace(r':\d*$', '', hostname)
 
         try:
             # Attempt to get a site based on the current domain
             request.site = Site.objects.get(domain=hostname)
+
         except Site.DoesNotExist:
             # If this site doesn't exist, revert to our SITE_ID
             request.site = Site.objects.get_current()
